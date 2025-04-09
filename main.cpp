94d141bc7c8ce921d5f245a19d11eeb096a055ff
Arsenii Evstigneev <logrus.928@gmail.com>
2025-04-10 01:02:55 +0200

    add ray coloring

--- main.cpp
+++ main.cpp
@@ -1,12 +1,57 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
+/*
+
+Центр камеры — это точка в 3D-пространстве, из которой исходят все лучи сцены.
+Её ещё называют "точкой глаза" (eye point), потому что она моделирует позицию наблюдателя.
+В терминах рейтрейсинга это точка O (origin) для всех лучей P(t) = O + t * D.
+
+Вектор от центра камеры до центра viewport (виртуального окна) ортогонален самому viewport,
+то есть перпендикулярен его плоскости.
+Это значит, что камера "смотрит" прямо на центр viewport, и направление взгляда 
+(вектор N) составляет угол 90° с плоскостью окна.
+
+*/
+
+color ray_color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return ((1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0));
+}
+
 int main(void)
 {
-	int	image_width = 256;
-	int	image_height = 256;
+
+	auto	aspect_ratio = 16.0 / 9.0;
+	int		image_width = 400;
+
+	/* Задаются переменные вроде image_width (ширина) и image_height (высота)
+	    — это количество пикселей по горизонтали и вертикали. */
+	int		image_height = int(image_width / aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
+
+	auto	focal_length = 1.0;
+	auto	viewport_height = 2.0;
+	auto	viewoprt_width = viewport_height * (double(image_width) / image_height);
+	auto	camera_center = point3(0, 0, 0);
+
+	auto	view_port_u = vec3(viewoprt_width, 0, 0);
+	auto	view_port_v = vec3(0, -viewport_height, 0);
+
+	/* Дельта-вектор — это вектор, который показывает, как перейти от одной точки к другой. 
+	Он получается, когда ты вычитаешь координаты одной точки из координат другой */
+	auto	pixel_delta_u = view_port_u / image_width;
+	auto	pixel_delta_v = view_port_v / image_height;
+
+	auto	viewport_upper_left = (camera_center - vec3(0, 0, focal_length)
+				- view_port_u / 2 - view_port_v / 2);
+	auto	pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+	
+	// Render
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
@@ -15,7 +60,11 @@ int main(void)
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
+			auto	pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto	ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
+
+			color	pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color);
 		}
 	}
