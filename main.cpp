8cfedecd0af77f3158d294488826e2186e46930d
Arsenii Evstigneev <logrus.928@gmail.com>
2025-04-10 20:45:33 +0200

    feat: add hitting sphere with rays

--- main.cpp
+++ main.cpp
@@ -16,12 +16,23 @@
 (вектор N) составляет угол 90° с плоскостью окна.
 
 */
+bool	hit_sphere(const point3& center, double radius, const ray&r) {
+	vec3 oc = center -r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto descriminant = b*b - 4*a*c;
+	return (descriminant >=0);
+}
 
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return (color(1.0, 0.0, 0.0));
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return ((1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0));
 }
+
 
 int main(void)
 {
@@ -30,16 +41,16 @@ int main(void)
 	int		image_width = 400;
 
 	/* Задаются переменные вроде image_width (ширина) и image_height (высота)
-	    — это количество пикселей по горизонтали и вертикали. */
+	— это количество пикселей по горизонтали и вертикали. */
 	int		image_height = int(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
 
 	auto	focal_length = 1.0;
 	auto	viewport_height = 2.0;
-	auto	viewoprt_width = viewport_height * (double(image_width) / image_height);
+	auto	viewport_width = viewport_height * (double(image_width) / image_height);
 	auto	camera_center = point3(0, 0, 0);
 
-	auto	view_port_u = vec3(viewoprt_width, 0, 0);
+	auto	view_port_u = vec3(viewport_width, 0, 0);
 	auto	view_port_v = vec3(0, -viewport_height, 0);
 
 	/* Дельта-вектор — это вектор, который показывает, как перейти от одной точки к другой. 
@@ -47,6 +58,7 @@ int main(void)
 	auto	pixel_delta_u = view_port_u / image_width;
 	auto	pixel_delta_v = view_port_v / image_height;
 
+	// Можно будет потом написать функцию которая находит средний вектор
 	auto	viewport_upper_left = (camera_center - vec3(0, 0, focal_length)
 				- view_port_u / 2 - view_port_v / 2);
 	auto	pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
